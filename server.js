7931f7f98fc07f8db1379f25dfe0c9741a11fb47
rafael silva <rafael.academianow@gmail.com>
2021-08-04 09:57:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+
+app.get("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+    res.json(client);
+});
+
+
+app.listen(3000, function(){
+    console.log("server is running");
+});
